3de7bc0e3f2b7f1e911c2261b54d8ac37cd9623d
giovani <giovanijfc@gmail.com>
2021-07-15 16:39:55 -0300

    fix: types

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import CommunicationBridge from 'communication-bridge';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
